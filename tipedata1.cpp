418675ee5d984650534ca77c2e3b3f2905eaa3e5
muhammad sultan <budionosultan8@gmail.com>
2025-11-21 07:50:55 +0700

    Add files via upload

---
+++ tipedata1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short angka1;
+    int angka2;
+    long angka3;
+
+    short angka1 = 6645;
+    int angka2 = 89;
+    long angka3 = 281234567;
+
+    cout << "angka1 (short) pendek: " << angka1 << endl;
+    cout << "angka (int) niali : " << angka2 << endl;
+    cout << "angka (long) panjang: " << angka3 << endl;
+
+    return 0;
+}

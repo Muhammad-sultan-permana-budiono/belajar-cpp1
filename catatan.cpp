ae0cf7c4796e5bc701b0bc7a1e58fa4402698e45
muhammad sultan <budionosultan8@gmail.com>
2025-11-21 14:37:47 +0700

    Add files via upload

---
+++ catatan.cpp
@@ -0,0 +1,13 @@
+//operator dalam c++
+
+//operator unary: yaitu terdiri dari 1 operand, contohnya:
+// operator positif(plus) +4, +2, +11.00 dll.
+
+// operator binary: operator terdiri dari 2 operand , contohnya
+// 6+3, 9*3, 10 - 4 dll
+
+//operator ternary yaitu terdiri dari 3 operand contohnya yaitu :(?:),
+//seperti (a > b) ? a : b:
+
+// (a > b) ini merupakan kondisi
+// a:b jadi a itu true dan b itu false
